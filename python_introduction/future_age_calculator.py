979a8540f3d0224a944ef287bfd9edc294a2b031
mtsdb <tomatias.te@gmail.com>
2025-09-08 02:50:50 +0300

    the future_age_calculator.py python project done

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+input_age = int(input("How old are you? "))
+
+future_year = 2050
+current_year = 2023
+years_to_add = future_year - current_year
+future_age = input_age + years_to_add
+
+print(f"In {future_year}, you will be {future_age} years old.")
