5edeb8f0225ee327699c2711a7201140f9db25d6
mtsdb <tomatias.te@gmail.com>
2025-09-08 03:04:09 +0300

    the finance_calculator.py python project update

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
-input_income = float(input("Enter your monthly income: "))
-input_expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings = input_income - input_expenses
+monthly_savings = monthly_income - monthly_expenses
 
-interest_rate = 0.05
-annual_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
+annual_savings = monthly_savings * 12
+projected_savings = annual_savings + (annual_savings * 0.05)
 
-print(f"Your monthly savings are {monthly_savings}")
-print(f"Projected savings after one year, with interest, is: {annual_savings}")
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
